600e7ca0837cfb84dd559fef13de3c5bc5a3ac29
Fei Qi <feiqi9047@gmail.com>
2018-11-27 21:14:08 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,8 @@
 def turn_count(board)
   counter = 0 
   board.each do |turns| 
-    if turns == ("X" || "O")
+   turns == ("X" || "O")
       counter +=1 
       return counter
-    else 
-      NIL
-    end
   end
 end
